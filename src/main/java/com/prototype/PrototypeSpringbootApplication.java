82a2590879c657796003ef8ea3d2509a3fd30608
YiyuanShang <yiyuan.shang@mail.mcgill.ca>
2021-06-25 14:16:57 -0400

    renamed package

--- src/main/java/com/prototype/PrototypeSpringbootApplication.java
+++ src/main/java/com/prototype/PrototypeSpringbootApplication.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.prototype;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/com/prototype/ServletInitializer.java
+++ src/main/java/com/prototype/ServletInitializer.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.prototype;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
