6ffc2707c0575a4a4dc7e842fb5352f048017e30
YiyuanShang <yiyuan.shang@mail.mcgill.ca>
2021-06-27 10:36:39 -0400

    finished initial configurations for logging

--- src/main/java/com/prototype/controller/Controller.java
+++ src/main/java/com/prototype/controller/Controller.java
@@ -1,14 +1,23 @@
 package com.prototype.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.prototype.entity.Person;
 
+
 @RestController
 public class Controller {
+	private static final Logger LOGGER = LoggerFactory.getLogger(Controller.class);
+	
 	@GetMapping("/string")
 	public String getString() {
+		LOGGER.debug("This is a debug message");
+		LOGGER.info("This is a info message");
+		LOGGER.warn("This is a warn message");
+		LOGGER.error("This is a error message");
 		return "returned result from getString()";
 	}
 
