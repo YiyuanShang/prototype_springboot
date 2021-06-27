c57a679875de12ff5bb15839de05de4a9c86a9b0
YiyuanShang <yiyuan.shang@mail.mcgill.ca>
2021-06-27 12:06:27 -0400

    finished initial implementation of profiles

---
+++ src/main/java/com/prototype/config/DevConfiguration.java
@@ -0,0 +1,9 @@
+package com.prototype.config;
+
+import org.springframework.context.annotation.Profile;
+
+@Profile("dev")
+public class DevConfiguration {
+	
+	
+}
--- src/main/java/com/prototype/controller/Controller.java
+++ src/main/java/com/prototype/controller/Controller.java
@@ -2,6 +2,7 @@ package com.prototype.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,13 +13,17 @@ import com.prototype.entity.Person;
 public class Controller {
 	private static final Logger LOGGER = LoggerFactory.getLogger(Controller.class);
 	
+	@Value("${app.message}")
+	public String appMessage;
+	
 	@GetMapping("/string")
 	public String getString() {
 		LOGGER.debug("This is a debug message");
 		LOGGER.info("This is a info message");
 		LOGGER.warn("This is a warn message");
 		LOGGER.error("This is a error message");
-		return "returned result from getString()";
+		
+		return appMessage;
 	}
 
 	@GetMapping("/object")
